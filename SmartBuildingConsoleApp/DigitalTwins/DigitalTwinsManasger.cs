bf24582781c15fb0876cc0bc9b305b3fcd691bbc
SBCskkuk <tjdnftkadbr@skkukdp.re.kr>
2024-01-24 15:35:10 +0900

    초기 커밋.

---
+++ SmartBuildingConsoleApp/DigitalTwins/DigitalTwinsManasger.cs
@@ -0,0 +1,30 @@
+﻿using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using System;
+using System.IO;
+namespace SmartBuildingConsoleApp.DigitalTwins
+{
+    public class DigitalTwinsManager
+    {
+        private static readonly string adtInstanceUrl = "<https://SBC.api.krc.digitaltwins.azure.net>";
+        private DigitalTwinsClient client;
+        public DigitalTwinsManager()
+        {
+            Connect();
+        }
+        public void Connect()
+        {
+            var cred = new DefaultAzureCredential();
+            client = new DigitalTwinsClient(new Uri(adtInstanceUrl), cred);
+        }
+
+        public void CreateModel(string path)
+        {
+            using var modelStreamReader = new StreamReader(path);
+            string dtdl = modelStreamReader.ReadToEnd();
+            string[] dtdls = new string[] { dtdl };
+            client.CreateModels(dtdls);
+        }
+
+    }
+}
---
+++ SmartBuildingConsoleApp/DigitalTwins/Program.cs
@@ -0,0 +1,17 @@
+﻿using SmartBuildingConsoleApp.DigitalTwins;
+using System;
+
+namespace SmartBuildingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalTwinsManager dtHelper = new DigitalTwinsManager();
+            // TODO
+
+            string path = "Models/Chapter4/room.json";
+            dtHelper.CreateModel(path);
+        }
+    }
+}
